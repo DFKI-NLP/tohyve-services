ccd3a8ea8324b236532f8f5cd149bd83c7a7919b
Akhyar Ahmed <nuhasahmed007@gmail.com>
2023-05-31 16:14:26 +0200

    Update transcribe.py

--- automatic_speech_recognition/transcribe.py
+++ automatic_speech_recognition/transcribe.py
@@ -1,6 +1,6 @@
 import gradio as gr
 import hyperlink
-
+import time
 
 # ASR Class
 class ASR:
@@ -10,7 +10,7 @@ class ASR:
         self.MODEL_NAME = MODEL_NAME
 
     # transcreibe method for both microphone streaming and audio file upload
-    def transcribe(self, Input_language, Microphone, File_upload):
+    def transcribe(self, Input_language, Microphone, File_upload, state=""):
         self.pipe.model.config.forced_decoder_ids = self.pipe.tokenizer.get_decoder_prompt_ids(language=Input_language, task="transcribe")
         warn_output = ""
         if (Microphone is not None) and (File_upload is not None):
@@ -18,15 +18,18 @@ class ASR:
                 "WARNING: You've uploaded an audio file and used the microphone. "
                 "The recorded file from the microphone will be used and the uploaded audio will be discarded.\n"
             )
-        elif (Microphone is None) and (File_upload is None):
-            return "ERROR: You have to either use the microphone or upload an audio file"
+        # elif (Microphone is None) and (File_upload is None):
+        #     return "ERROR: You have to either use the microphone or upload an audio file"
         elif Input_language is None:
             return "ERROR: You didn't set transcribed language code!\n"
 
         file = Microphone if Microphone is not None else File_upload
         text = self.pipe(file)["text"]
-
-        return warn_output + text
+        state += text
+        if Microphone:
+            return state, state
+        else:
+            return warn_output + text
 
 
     # method which launch Gradio to do ASR
@@ -48,21 +51,24 @@ class ASR:
             fn=self.transcribe,
             inputs=[
                 gr.components.Textbox(lines=1, placeholder="e.g. de or en", type="text"),
-                gr.components.Audio(source="microphone", type="filepath"),
+                gr.components.Audio(source="microphone", type="filepath", streaming=True),
                 gr.components.Audio(source="upload", type="filepath"),
+                "state",
             ],
-            outputs="text",
+            outputs=[ "textbox", "state"],
             css=css_code,
             title="Automatic Speech Recognition Demonstrator",
             description=(
-                f"""\n\nThis is the demonstrator for multilingual automatic speech recognition for the ToHyVe project. It supports static file upload (mp3 format) and audio streaming from the local microphone. We used this tool for English(en) and German(de) only. However, it supports many more languages. The amount of training data is different on different languages. Therefore, the quality differs from language to language. For more information on the amount of training data per language, please read {url.to_text()} (e.g. page 28). 
+                f"""\n\nThis is the demonstrator for multilingual automatic speech recognition for the ToHyVe project. It supports static file upload (mp3 format) and audio streaming from the local microphone. We used this tool for English(en) and German(de) only. However, it supports many more languages. The amount of training data is different on different languages. Therefore, the quality differs from language to language. For more information on the amount of training data per language, please read Redford et al. 2022({url.to_text()}) (e.g. page 28). 
                 This are the supported languages:
                 'en', 'zh', 'de', 'es', 'ru', 'ko', 'fr', 'ja', 'pt', 'tr', 'pl', 'ca', 'nl', 'ar', 'sv', 'it', 'id', 'hi', 'fi', 'vi','he', 'uk', 'el', 'ms', 'cs', 'ro', 'da', 'hu', 'ta', 'no', 'th', 'ur', 'hr', 'bg', 'lt', 'la', 'mi', 
                 'ml', 'cy', 'sk', 'te', 'fa', 'lv', 'bn', 'sr', 'az', 'sl', 'kn', 'et', 'mk', 'br', 'eu', 'is', 'hy', 'ne', 'mn', 'bs', 'kk', 'sq', 'sw',  'gl', 'mr', 'pa', 'si', 'km', 'sn', 'yo', 'so', 'af', 'oc', 'ka', 'be', 'tg', 'sd', 
                 'gu', 'am', 'yi', 'lo', 'uz', 'fo', 'ht', 'ps', 'tk', 'nn', 'mt', 'sa', 'lb', 'my', 'bo', 'tl', 'mg', 'as', 'tt', 'haw', 'ln', 'ha', 'ba', 'jw', 'su', 'my', 'ca', 'nl', 'ht', 'lb', 'ps', 'pa', 'ro', 'ro', 'si', 'es'"""
             ),
+            live=True,
             allow_flagging="never",
             # auto_submit=True,
+            
         )
         # with demo:
         #     gr.TabbedInterface([mf_transcribe], ["Transcribe Audio"])
