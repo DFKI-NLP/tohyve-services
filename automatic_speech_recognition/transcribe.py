cc4b37412dd8bae891cded14c9c46844dbd9d351
Akhyar Ahmed <nuhasahmed007@gmail.com>
2023-06-21 10:25:21 +0200

    Update transcribe.py

--- automatic_speech_recognition/transcribe.py
+++ automatic_speech_recognition/transcribe.py
@@ -47,7 +47,7 @@ class ASR:
 
     # method which launch Gradio to do ASR
     def do_asr(self):
-        url = hyperlink.parse(u"https://arxiv.org/pdf/2212.04356.pdf")
+        url = hyperlink.parse(u"https://github.com/DFKI-NLP/tohyve-services/tree/master/automatic_speech_recognition")
         css_code = 'div {\
             margin-left:auto;\
             margin-right:auto;\
@@ -59,11 +59,12 @@ class ASR:
             }'
         title = "Automatic Speech Recognition Demonstrator"
         description = (
-                f"""\n\nThis is the demonstrator for multilingual automatic speech recognition for the ToHyVe project. It supports static file upload (mp3 format) and audio streaming from the local microphone. We used this tool for English(en) and German(de) only. However, it supports many more languages. The amount of training data is different on different languages. Therefore, the quality differs from language to language. For more information on the amount of training data per language, please read Redford et al. 2022({url.to_text()}) (e.g. page 28). 
-                This are the supported languages:
+                f"""\n\nThis is the demonstrator for multilingual automatic speech recognition for the ToHyVe project. It supports static file upload (mp3 and wav format) and audio streaming from the local microphone. 
+                To use the online demo please specify the language. Supported languages are:
                 'en', 'zh', 'de', 'es', 'ru', 'ko', 'fr', 'ja', 'pt', 'tr', 'pl', 'ca', 'nl', 'ar', 'sv', 'it', 'id', 'hi', 'fi', 'vi','he', 'uk', 'el', 'ms', 'cs', 'ro', 'da', 'hu', 'ta', 'no', 'th', 'ur', 'hr', 'bg', 'lt', 'la', 'mi', 
                 'ml', 'cy', 'sk', 'te', 'fa', 'lv', 'bn', 'sr', 'az', 'sl', 'kn', 'et', 'mk', 'br', 'eu', 'is', 'hy', 'ne', 'mn', 'bs', 'kk', 'sq', 'sw',  'gl', 'mr', 'pa', 'si', 'km', 'sn', 'yo', 'so', 'af', 'oc', 'ka', 'be', 'tg', 'sd', 
-                'gu', 'am', 'yi', 'lo', 'uz', 'fo', 'ht', 'ps', 'tk', 'nn', 'mt', 'sa', 'lb', 'my', 'bo', 'tl', 'mg', 'as', 'tt', 'haw', 'ln', 'ha', 'ba', 'jw', 'su', 'my', 'ca', 'nl', 'ht', 'lb', 'ps', 'pa', 'ro', 'ro', 'si', 'es'"""
+                'gu', 'am', 'yi', 'lo', 'uz', 'fo', 'ht', 'ps', 'tk', 'nn', 'mt', 'sa', 'lb', 'my', 'bo', 'tl', 'mg', 'as', 'tt', 'haw', 'ln', 'ha', 'ba', 'jw', 'su', 'my', 'ca', 'nl', 'ht', 'lb', 'ps', 'pa', 'ro', 'ro', 'si', 'es'
+                \nMore information can be found in the technical documentation({url.to_text()})."""
             )
         demo = gr.Blocks(css=".gradio-container {background: url('https://www.dfki.de/fileadmin/user_upload/DFKI/Medien/Logos/Logos_DFKI/DFKI_Logo.png'); background-repeat: no-repeat; background-attachment:absolute; background-position: 30px 50px; background-size: 90px 45px;}")
         micro_transcribe = gr.Interface(
