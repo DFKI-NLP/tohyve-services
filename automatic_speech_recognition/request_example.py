9290ca014cf9343b6a973894a39ae02e23f4e8b4
Akhyar Ahmed <nuhasahmed007@gmail.com>
2023-06-22 10:08:24 +0200

    Create request_example.py

---
+++ automatic_speech_recognition/request_example.py
@@ -0,0 +1,26 @@
+import requests
+import base64
+import json
+
+
+audio_path = "C:/Users/ASUS/Downloads/speech_de.mp3"
+
+with open(audio_path, "rb") as f:
+    base64_encoded_audio = base64.b64encode(f.read())
+    base64_encoded_audio = str(base64_encoded_audio)[2:-1]
+
+    headers = {"Content-Type": "application/json"}
+    request_body = {
+        "fn_index":3,
+        "data": [
+            "de",
+            {
+                "data":"data:audio/wav;base64," + str(base64_encoded_audio),
+                "name": "sample_audio.mp3"
+            }
+        ]
+    }
+    url = "https://dfki-3109.dfki.de/run/predict"
+    response = requests.post(url, json=request_body, headers=headers)
+
+    print(json.dumps(response.json(), indent=4))
