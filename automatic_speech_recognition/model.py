1ae8b09172559210a3a1ce097f477decc516a4c4
Akhyar Ahmed <nuhasahmed007@gmail.com>
2023-08-03 11:21:07 +0200

    ASR: completed

--- automatic_speech_recognition/model.py
+++ automatic_speech_recognition/model.py
@@ -6,21 +6,31 @@ from transformers import pipeline
 
 # initiate the whisper model
 def init_model():
-    # device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
     MODEL_NAME = "openai/whisper-medium" # we can change this name to change whisper model type
     path = "models/"
-    model_path = path+MODEL_NAME.split("/")[1]+".pkl"
 
-    if os.path.exists(model_path):
-        pipe =  pickle.load(open(model_path, 'rb'))
+    if os.path.exists(path): # this is for my local env
+        pipe = pipeline(
+        task="automatic-speech-recognition",
+        model=path+"/whisper-medium",
+        chunk_length_s=30,
+        device=device
+        )
+    elif os.path.exists("/app/"+path): # this is for docker env
+        pipe = pipeline(
+        task="automatic-speech-recognition",
+        model="/app/"+path+"whisper-medium",
+        chunk_length_s=30,
+        device=device
+        )
     else:
         pipe = pipeline(
             task="automatic-speech-recognition",
             model=MODEL_NAME,
-            chunk_length_s=30
+            chunk_length_s=30,
+            device = device
         )
-        pickle.dump(pipe, open(model_path, 'wb'))
     
-    # pipe.device = device
     return pipe, MODEL_NAME
 
