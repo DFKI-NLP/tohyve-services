2f81252f067171fcbb6ebd0e0a7d12e74ef55b24
Akhyar Ahmed <nuhasahmed007@gmail.com>
2023-10-26 18:02:57 +0200

    FileUpload Feature Added

--- tohyve_demo/main.py
+++ tohyve_demo/main.py
@@ -2,26 +2,29 @@ import uvicorn
 
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
 from fastapi.responses import HTMLResponse
-# from fastapi.staticfiles import StaticFiles
+# from fastapi.responses import FileResponse
+# # from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from websocket_ import ConnectionManager
 
 app = FastAPI()
 
+# # Mount the "static" directory to serve static files (JavaScript)
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Mount the directory containing your HTML files
-# app.mount("/static", StaticFiles(directory="./templates", html=True), name="static")
 templates = Jinja2Templates(directory="templates")
-
-
 
 manager = ConnectionManager()
 
+
+# Endpoint to serve the HTML file
 @app.get("/tohyve-demo",response_class=HTMLResponse)
 async def get(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-        
 
+
+# Endpoint for the websocket
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await manager.connect(websocket)
@@ -31,10 +34,6 @@ async def websocket_endpoint(websocket: WebSocket):
             await manager.send_personal_message(json_data, websocket)  
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-
-
-
-
 
 
 # Start the WebSocket server in a separate thread
