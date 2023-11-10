c0268a2f774b902d7b8c737380e24686bbaa62e1
Akhyar Ahmed <nuhasahmed007@gmail.com>
2023-11-10 14:46:34 +0100

    Updated DEMO

--- tohyve_demo/main.py
+++ tohyve_demo/main.py
@@ -2,12 +2,24 @@ import uvicorn
 
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 # from fastapi.responses import FileResponse
 # # from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from websocket_ import ConnectionManager
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # # Mount the "static" directory to serve static files (JavaScript)
 # app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -17,11 +29,10 @@ templates = Jinja2Templates(directory="templates")
 
 manager = ConnectionManager()
 
-
 # Endpoint to serve the HTML file
 @app.get("/tohyve-demo",response_class=HTMLResponse)
 async def get(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("sample.html", {"request": request})
 
 
 # Endpoint for the websocket
@@ -41,5 +52,5 @@ if __name__ == '__main__':
         uvicorn.run(
         "main:app",
         host    = "0.0.0.0",
-        port    = 8005
+        port    = 8081
     )
