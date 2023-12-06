4c1b04a65c5761afb21471537e4e91d342649549
Akhyar Ahmed <nuhasahmed007@gmail.com>
2023-12-06 23:23:22 +0100

    Create sample.py

---
+++ automatic_speech_recognition/sample.py
@@ -0,0 +1,54 @@
+import requests
+import base64
+import json
+import asyncio
+
+
+async def start_stream(url, language_code):
+    # Make a GET request with stream=True to enable streaming
+    response = requests.get(url, stream=True)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        chunk_ls = []
+        # Process the streaming data in chunks
+        for chunk in response.iter_content(chunk_size=102400):
+            if chunk:
+                # Process each chunk as needed
+                # Encode bytes to Base64 
+                base64_encoded_chunk = base64.b64encode(chunk)
+            
+                # If you want the result as a string
+                base64_encoded_audio = base64_encoded_chunk.decode('utf-8')
+                
+                # Process the Base64 encoded chunk as needed
+                asr_response = await send_asr(base64_encoded_audio, language_code)
+                print(json.dumps(asr_response.json(), indent=4))
+
+    else:
+        print(f"Failed to fetch streaming data. Status code: {response.status_code}")
+
+
+async def send_asr(chunks, language_code):
+    headers = {"Content-Type": "application/json"}
+    request_body = {
+        "fn_index":4,
+        # "fn_index":3,
+        "data": [
+            str(language_code), # this need to be change according to the audio language code 
+            {
+                "data":"data:audio/wav;base64," + chunks,
+                "name":"sample_audio.mp3"
+            }
+        ]
+    }
+    url = "https://dfki-3109.dfki.de/asr/run/predict"
+    response = requests.post(url, json=request_body, headers=headers)
+    return response
+
+
+# Start streaming
+if __name__ == '__main__':
+    stream_url = 'https://st01.sslstream.dlf.de/dlf/01/128/mp3/stream.mp3?aggregator=web'
+    language_code = "de"
+    asyncio.run(start_stream(stream_url, language_code))
