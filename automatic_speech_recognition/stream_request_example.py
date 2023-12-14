abe11d1c5a5b6c54dccf962fc281313b4b9ef24f
Akhyar Ahmed <nuhasahmed007@gmail.com>
2023-12-15 00:38:43 +0100

    Update stream_request_example.py

--- automatic_speech_recognition/stream_request_example.py
+++ automatic_speech_recognition/stream_request_example.py
@@ -1,8 +1,8 @@
 import requests
 import json
 
-url = "https://dfki-3109.dfki.de/asr/web-stream"
-# url = "http://localhost:8001/asr/web-stream"
+# url = "https://dfki-3109.dfki.de/asr/web-stream"
+url = "http://localhost:8001/asr/web-stream"
 params = {
     "url": "https://st01.sslstream.dlf.de/dlf/01/128/mp3/stream.mp3?aggregator=web",
     "source_language": "de"
@@ -15,28 +15,19 @@ if response.status_code == 200:
     if response.iter_content():
         for i, line in enumerate(response.iter_lines()):
             if line:
-                decoded_line = line.decode('utf-8')
-                print(decoded_line)
-                responses_ls.append(decoded_line)
+                response_str = line.decode('utf-8')
+                if "}{" in response_str:
+                    responses_ls.append(response_str[0])
+                    responses_str = " ".join(responses_ls)
+                    full_response = json.dumps(json.loads(responses_str), indent=4)
+                    print(full_response)
+                    responses_ls = []
+                    responses_ls.append(response_str[1])
+                else:
+                    responses_ls.append(response_str)
     else:
         print("No content received from the server.")
 else:
     print(f"Request failed with status code {response.status_code}")
 
 
-
-
-
-
-
-
-# response_str = line.decode('utf-8')
-# if "}{" in response_str:
-#     responses_ls.append(response_str[0])
-#     responses_str = " ".join(responses_ls)
-#     full_response = json.dumps(json.loads(responses_str), indent=4)
-#     print(full_response)
-#     responses_ls = []
-#     responses_ls.append(response_str[1])
-# else:
-#     responses_ls.append(response_str)
